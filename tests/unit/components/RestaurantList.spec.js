982a6785fb08e0e0a1c5b146c9da8d52123abcdf
Guido Van Hoof <guido.van.hoof3@telenet.be>
2020-11-17 21:37:03 +0100

    Displays the restaurants

--- tests/unit/components/RestaurantList.spec.js
+++ tests/unit/components/RestaurantList.spec.js
@@ -23,4 +23,38 @@ describe('RestaurantList', () => {
 
     expect(restaurantsModule.actions.load).toHaveBeenCalled();
   });
+
+  it('displays the restaurants', () => {
+    const records = [
+      {id: 1, name: 'Sushi Place'},
+      {id: 2, name: 'Pizza Place'},
+    ];
+
+    const restaurantsModule = {
+      namespaced: true,
+      state: {records},
+      actions: {
+        load: jest.fn().mockName('load'),
+      },
+    };
+    const store = new Vuex.Store({
+      modules: {
+        restaurants: restaurantsModule,
+      },
+    });
+
+    const component = mount(RestaurantList, {localVue, store});
+
+    const firstRestaurantName = component
+      .findAll('[data-testid="restaurant"]')
+      .at(0)
+      .text();
+    expect(firstRestaurantName).toBe('Sushi Place');
+
+    const secondRestaurantName = component
+      .findAll('[data-testid="restaurant"]')
+      .at(1)
+      .text();
+    expect(secondRestaurantName).toBe('Pizza Place');
+  });
 });
