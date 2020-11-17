4686bb00ffea3294c2ff33fc0fecf3c981d6d5c2
Guido Van Hoof <guido.van.hoof3@telenet.be>
2020-11-17 20:55:44 +0100

    Load restaurants upon mounting RestaurantList

--- src/components/RestaurantList.vue
+++ src/components/RestaurantList.vue
@@ -5,7 +5,15 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex';
+
 export default {
   name: 'RestaurantList',
+  mounted() {
+    this.loadRestaurants();
+  },
+  methods: mapActions({
+    loadRestaurants: 'restaurants/load',
+  }),
 };
 </script>
